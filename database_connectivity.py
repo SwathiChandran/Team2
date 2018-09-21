7acdc06cb1f05fc979ad51a5ad4864753f2f1583
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-09-21 12:35:22 +0530

    Comaparing table and csv -Praveen

--- database_connectivity.py
+++ database_connectivity.py
@@ -12,11 +12,7 @@ def database_connectivity():
 		return conn
 	except Exception as error:
 		print error
-sample=database_connectivity()
-if sample:
-		print "connection successfull"	
-else:
-		print "no"
+
 		
 # configuration file reading	 	
 
@@ -34,7 +30,7 @@ def config_file_read():
 	except Exception as error:
 		print error
 
-config_file_read()
+
 
 #reading csv file
 def reading_csv():
@@ -46,4 +42,46 @@ def reading_csv():
             csv_list.append(line)
         print(csv_list)
     f.close()
+
+
+#Comparing table and csv 
+def table_csv_comparing():
+	csv_list=[]
+	table_list = []
+	database_name = raw_input('Enter the Database Name :')
+	csv_name=raw_input("enter the csv file name:")
+	database_name = database_name+".db"
+	conn = sqlite3.connect(database_name)
+	c= conn.cursor()
+	table_values = c.execute("SELECT * FROM cricket")
+	for i in table_values:
+		table_list.append(list(i))
+	print table_list
+	
+	with open(csv_name,"rb") as f:
+		reader=csv.reader(f,delimiter=",")
+		for line in reader:
+			csv_list.append(line)
+	print(csv_list)
+	
+	flag = 0
+		for i in csv_list:
+			if i in table_list:
+				flag+=1
+		print "Flag is :",flag
+		if flag == len(table_list):
+			print "True"
+		else:
+			print "False"
+			
+	f.close()
+	conn.close()
+	
+sample=database_connectivity()
+if sample:
+		print "connection successfull"	
+else:
+		print "no"	
+config_file_read()
 reading_csv()
+table_csv_comparing()
