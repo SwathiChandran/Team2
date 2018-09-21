043d98cc06f750c9433b236098747909a64517e4
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-09-21 12:30:13 +0530

    Reading csv --roja

--- database_connectivity.py
+++ database_connectivity.py
@@ -35,3 +35,15 @@ def config_file_read():
 		print error
 
 config_file_read()
+
+#reading csv file
+def reading_csv():
+    csv_name=raw_input("enter the csv file name:")
+    csv_list=[]
+    with open(csv_name,"rb") as f:
+        reader=csv.reader(f,delimiter=",")
+        for line in reader:
+            csv_list.append(line)
+        print(csv_list)
+    f.close()
+reading_csv()
